1049b0653cb02f8bebd4e274aa38ded62318ccdc
Md.Rezaul Karim <rezaul181211@outlook.com>
2020-02-15 12:21:10 +0600

    blog archive

--- single.php
+++ single.php
@@ -17,7 +17,7 @@
   <div class="container container--narrow page-section">
   
      <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Block Home </a> 
+      <p><a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i>Back to Blog</a> 
 	  <span class="metabox__main">Posted by <?php the_author_posts_link(); ?> on <?php the_time('F-m-Y'); ?> in <?php echo get_the_category_list(', '); ?></span>
 	  </p>
     </div>
